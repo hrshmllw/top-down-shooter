d339fb6220799d994c93f390177b9b2a4eb34d0a
hrshmllw <ejmac041196@gmail.com>
2022-01-24 14:10:03 +0800

    added mobs, movement, shooting

--- main.py
+++ main.py
@@ -18,18 +18,25 @@ class Game:
         img_folder = path.join(game_folder, 'img')
         self.map = Map(path.join(game_folder, 'map.txt'))
         self.player_img = pg.image.load(path.join(img_folder, PLAYER_IMG)).convert_alpha()
+        self.bullet_img = pg.image.load(path.join(img_folder, BULLET_IMG)).convert_alpha()
+        self.mob_img = pg.image.load(path.join(img_folder, MOB_IMG)).convert_alpha()
+        self.wall_img = pg.image.load(path.join(img_folder, WALL_IMG)).convert_alpha()
+        self.wall_img = pg.transform.scale(self.wall_img, (TILESIZE, TILESIZE))
 
     def new(self):
         self.all_sprites = pg.sprite.Group()
         self.walls = pg.sprite.Group()
+        self.mobs = pg.sprite.Group()
+        self.bullets = pg.sprite.Group()
         for row, tiles in enumerate(self.map.data):
             for col, tile in enumerate(tiles):
                 if tile == '1':
                     Wall(self, col, row)
+                if tile == 'M':
+                    Mob(self, col, row)
                 if tile == 'S':
                     self.player = Player(self, col, row)
         self.camera = Camera(self.map.width, self.map.height)
-
 
     def run(self):
         # game loop - set self.playing = False to end the game
@@ -48,6 +55,10 @@ class Game:
         self.all_sprites.update()
         self.camera.update(self.player)
 
+        hits = pg.sprite.groupcollide(self.mobs, self.bullets, False, True)
+        for hit in hits:
+            hit.kill()
+
     def draw_grid(self):
         for x in range(0, WIDTH, TILESIZE):
             pg.draw.line(self.screen, LIGHTGREY, (x, 0), (x, HEIGHT))
@@ -55,8 +66,9 @@ class Game:
             pg.draw.line(self.screen, LIGHTGREY, (0, y), (WIDTH, y))
 
     def draw(self):
+        pg.display.set_caption("{:.2f}".format(self.clock.get_fps()))
         self.screen.fill(BGCOLOR)
-        self.draw_grid()
+        # self.draw_grid()
         for sprite in self.all_sprites:
             self.screen.blit(sprite.image, self.camera.apply(sprite))
         pg.display.flip()
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 import pygame as pg
+vec = pg.math.Vector2
 
 # color definitions
 WHITE = (255, 255, 255)
@@ -9,12 +10,13 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
+BROWN = (106, 55, 5)
 
 # game settings
 WIDTH = 1024
 HEIGHT = 768
-FPS = 30
-BGCOLOR = DARKGREY
+FPS = 60
+BGCOLOR = LIGHTGREY
 TILESIZE = 64
 GRIDWIDTH = WIDTH/TILESIZE
 GRIDHEIGHT = HEIGHT/TILESIZE
@@ -24,3 +26,20 @@ PLAYER_SPEED = 300
 PLAYER_ROT_SPEED = 250
 PLAYER_IMG = 'player_sprite.png'
 PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
+BARREL_OFFSET = vec(30, 0)
+
+# gun settings
+BULLET_IMG = 'projectile.png'
+BULLET_SPEED = 500
+BULLET_LIFETIME = 1000
+BULLET_RATE = 150
+RECOIL = 200
+GUN_SPREAD = 5
+
+# mob settings
+MOB_IMG = 'mob_sprite.png'
+MOB_SPEED = 150
+MOB_HIT_RECT = pg.Rect(0, 0, 30, 30)
+
+# environment settings
+WALL_IMG = 'wall_sprite.png'
