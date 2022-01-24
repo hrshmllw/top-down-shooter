263f7138a8722b33c21c0a46a0671f122c46ee18
hrshmllw <ejmac041196@gmail.com>
2022-01-24 09:49:22 +0800

    initial commit

---
+++ settings.py
@@ -0,0 +1,26 @@
+import pygame as pg
+
+# color definitions
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 1024
+HEIGHT = 768
+FPS = 30
+BGCOLOR = DARKGREY
+TILESIZE = 64
+GRIDWIDTH = WIDTH/TILESIZE
+GRIDHEIGHT = HEIGHT/TILESIZE
+
+# player settings
+PLAYER_SPEED = 300
+PLAYER_ROT_SPEED = 250
+PLAYER_IMG = 'player_sprite.png'
+PLAYER_HIT_RECT = pg.Rect(0, 0, 35, 35)
